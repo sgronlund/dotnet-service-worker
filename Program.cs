ac286f8fe0b93d359d8d9f7acd48252ef832e59d
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-01-14 17:22:01 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using dotnet_service_worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
